881e1afca07265e937241d365fa9d0d4432cc711
alm1rdev <silvaalmir556@gmail.com>
2025-08-10 12:23:07 -0300

    exercicio4

---
+++ exercicio4.py
@@ -0,0 +1,6 @@
+caderno= 15
+caneta=2.5
+uni1=int(input("quantos cadernos voce comprou?"))
+uni2=int(input("e quantas canetas?"))
+soma=(uni1*caderno)+(uni2*caneta)
+print(f"o total da compra foi de {soma} reais!")
