b43e5c78021932591421f9e893ec868fcbfbc074
alm1rdev <silvaalmir556@gmail.com>
2025-08-10 11:49:33 -0300

    exer3

--- exercicio2.py
+++ exercicio2.py
@@ -1,4 +1,4 @@
 vr=float(input('valor para conversao?'))
 cd=5.43
 result=vr / cd
-print(f'convertendo voce tem{result:.2f}dolares!')
+print(f'convertendo voce tem {result:.2f}dolares!')
---
+++ exercio3.py
@@ -0,0 +1,5 @@
+n1= float( 8.5)
+n2=float (7.0)
+n3= float (9.2)
+media= (n1 + n2 +n3)/3
+print(f"a media das notas e {media:.2f}")
