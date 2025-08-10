c65c6ef0e2905ae69bf77cd18688aaa13d81c2c9
alm1rdev <silvaalmir556@gmail.com>
2025-08-10 13:28:30 -0300

    exercicio-6

---
+++ exercicio6.py
@@ -0,0 +1,5 @@
+ola=input("ola, agoara vamos calcular seu imc,TECLE ENTER !")
+p1=float(input("qual seu peso em quilogramas?"))
+p2=float(input("qual sua altura em metros"))
+calculo=p1/(p2*p2)
+print(f"SEU IMC E DE : {calculo:.2f}. Se seu imc esta entre 18 e 25 parabens voce esta em bem! ATENCAO, abaixo de 18 e acima de 25 procure orientacao de um profissional da saude.")
